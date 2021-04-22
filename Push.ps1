8a788c1578580b3a2283f1d2b8066275ddc45eca
Andrew Piper <andrew@threax.com>
2021-04-22 10:53:48 -0400

    Added push script.

---
+++ Push.ps1
@@ -0,0 +1,8 @@
+git push
+git push --tags
+git push https://github.com/threax/Threax.AspNetCore.CSP.git
+git push https://github.com/threax/Threax.AspNetCore.CSP.git --tags
+git push https://gitlab.com/threax-software/Threax.AspNetCore.CSP.git
+git push https://gitlab.com/threax-software/Threax.AspNetCore.CSP.git --tags
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.AspNetCore.CSP
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.AspNetCore.CSP --tags
